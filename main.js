28d18e2bdabe73a0feb0fe9ba30e26f144f4475f
clinconao <cl.linconao@gmail.com>
2023-04-26 13:34:37 +1200

    Cambio comida tarjeta 1 Claudio

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ var receta2 = document.getElementById("r2");
 receta2.innerHTML = "Receta 2";
 var titulo = document.getElementById("cambio1");
 titulo.innerHTML = "Comidas más ricas";
-var titulocomida = document.getElementById("cambioclaudio1");
-titulo.innerHTML = "Pakus";
+var comida1 = document.getElementById("comidanueva");
+comida1.innerHTML = "Pakus";

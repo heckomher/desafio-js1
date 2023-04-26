f850a405ee8e91bc1d014ed744645ee54389d971
clinconao <cl.linconao@gmail.com>
2023-04-26 13:31:42 +1200

    Cambios comida 1 claudio

--- main.js
+++ main.js
@@ -4,3 +4,5 @@ var receta2 = document.getElementById("r2");
 receta2.innerHTML = "Receta 2";
 var titulo = document.getElementById("cambio1");
 titulo.innerHTML = "Comidas más ricas";
+var titulocomida = document.getElementById("cambioclaudio1");
+titulo.innerHTML = "Pakus";

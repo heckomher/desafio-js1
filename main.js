ced2cc13c3c77160c0767b435be8748d52716d14
Enriquebahamondelagos <enriquebahamonde@educom.cl>
2023-04-25 21:21:50 -0400

    correccion de titulo

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
 
 var receta2 = document.getElementById("r2");
-receta2.innerHTML = "Receta 2"
+receta2.innerHTML = "Receta 2";
+var titulo = document.getElementById("cambio1");
+titulo.innerHTML = "Comidas más ricas";

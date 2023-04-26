ef6db2d281423122773bc4ebcafa556b391c8d8c
clinconao <cl.linconao@gmail.com>
2023-04-26 14:20:32 +1200

    cambio nuevo claudio

--- main.js
+++ main.js
@@ -4,5 +4,12 @@ var receta2 = document.getElementById("r2");
 receta2.innerHTML = "Receta 2";
 var titulo = document.getElementById("cambio1");
 titulo.innerHTML = "Comidas más ricas";
+
 var comida1 = document.getElementById("comidanueva");
 comida1.innerHTML = "Pakus";
+var segundocambiocomida =document.getElementById("segundacomida")
+comidanueva.innerHTML = "Empanada de pino en jarra"
+var tercercambiocomida =document.getElementById("terceracomida")
+comidanueva3.innerHTML = "Ensalada"
+
+

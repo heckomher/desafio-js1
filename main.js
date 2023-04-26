6a21d81fc9469c4c93c0d8d14c6018a90cdf481e
Prision Mike <michael.pizarro.l@gmail.com>
2023-04-25 22:03:21 -0400

    cambio en el nav

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ var receta2 = document.getElementById("r2");
 receta2.innerHTML = "Receta 2";
 var titulo = document.getElementById("cambio1");
 titulo.innerHTML = "Comidas más ricas";
+
+var tituloMenu = document.getElementById("tituloNav");
+tituloMenu.innerHTML = "Manso Bajón";
+
+var tituloPrincipal = document.getElementById("linkIndex");
+tituloPrincipal.innerHTML = "Principal";
+
+var tituloContacto = document.getElementById("linkContacto");
+tituloContacto.innerHTML = "Contactar";
+
+var tituloBoton = document.getElementById("tituloBoton");
+tituloBoton.innerHTML = "Búsqueda";
